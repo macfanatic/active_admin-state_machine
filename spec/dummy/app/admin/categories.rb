b889fa42108df98b01e527e9fe06f60e5154ecc8
Matthew Millsaps-Brewer <matt@madebylotus.com>
2021-09-01 10:57:32 -0400

    Drops conditional code for AA < 1.0

--- spec/dummy/app/admin/categories.rb
+++ spec/dummy/app/admin/categories.rb
@@ -1,10 +1,5 @@
 ActiveAdmin.register Category do
-    action_item_args =  if ActiveAdmin::VERSION.start_with?('0.')
-                          [{ only: :show }]
-                        else
-                          ["state_action_category", { only: :show }]
-                        end
-    action_item(*action_item_args) do
+  action_item(:state_action_category, only: :show) do
     link_to "Posts", admin_category_posts_path(resource)
   end
 end
