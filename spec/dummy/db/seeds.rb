07cf835a4bda0d07ac9d881834307d0710df4474
Matthew Millsaps-Brewer <matt@madebylotus.com>
2021-09-01 10:57:38 -0400

    Adds super admin user to seeds

--- spec/dummy/db/seeds.rb
+++ spec/dummy/db/seeds.rb
@@ -1,1 +1,2 @@
-AdminUser.create!(:email => 'admin@example.com', :password => 'password', :password_confirmation => 'password') unless AdminUser.exists?(email: 'admin@example.com')
+AdminUser.create!(email: 'admin@example.com', password: 'password') unless AdminUser.exists?(email: 'admin@example.com')
+AdminUser.create!(email: 'super@example.com', password: 'password', super_admin: true) unless AdminUser.exists?(email: 'super@example.com')
